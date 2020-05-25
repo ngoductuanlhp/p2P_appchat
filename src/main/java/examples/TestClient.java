83c7a54ce47274c39a7ba95fb901e1195e08ad89
Khoa <truongkhoa.1799@gmail.com>
2020-05-25 22:57:39 +0700

    done core send file

--- src/main/java/examples/TestClient.java
+++ src/main/java/examples/TestClient.java
@@ -2,10 +2,12 @@ package examples;
 
 import backend.client.ChatClient;
 import controller.LoginController;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import ui.LoginUI;
 
 public class TestClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException{
         LoginUI loginUI = new LoginUI();
         ChatClient chatClient = new ChatClient("192.168.0.103", 11111);
         LoginController loginController = new LoginController(loginUI, chatClient);
