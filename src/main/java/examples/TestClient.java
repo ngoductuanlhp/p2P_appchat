877919a240e5c9b65e071ed614a2401af792a7e1
KhoaMac <truongkhoa.1799@gmail.com>
2020-06-15 20:33:34 +0700

    dsd

--- src/main/java/examples/TestClient.java
+++ src/main/java/examples/TestClient.java
@@ -11,7 +11,7 @@ import ui.LoginUI;
 public class TestClient {
     public static void main(String[] args) throws UnknownHostException{
         LoginUI loginUI = new LoginUI();
-        ChatClient chatClient = new ChatClient("192.168.0.111", 11111);
+        ChatClient chatClient = new ChatClient("192.168.1.98", 11111);
         LoginController loginController = new LoginController(loginUI, chatClient);
         loginController.initController();
     }
